366a392033769397c0737f0657c68cee6bf0fd94
Raynor <jonggu.woo@fitpet.co.kr>
2024-05-10 14:04:36 +0900

    ㅅㅂ

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Viewer Bot
 
+Proxy Server List
+
+* https://hidemyname.io/en/proxy-list/?country=JP&type=hs#list
 * https://www.proxynova.com/proxy-server-list/
--- app/http_method/http_proxy_servers.py
+++ app/http_method/http_proxy_servers.py
@@ -3,21 +3,27 @@ import requests
 from app.utils.proxy import Proxy
 
 # url = "https://sanggi-jayg.tistory.com/entry/Gradle-Gradle-dependency-%EA%B7%B8%EB%9E%98%EB%93%A4-%EC%A2%85%EC%86%8D%EC%84%B1-%EC%84%A0%EC%96%B8"
-url = "https://www.howsmyssl.com"
+# url = "https://www.howsmyssl.com"
+url = "https://httpbin.org/ip"
 
 if __name__ == "__main__":
-    # for proxy in PROXY_SERVERS:
-    for proxy in [Proxy("210.212.39.138", 8080)]:
+    # for proxy in [Proxy("47.74.7.51", 80)]:
+    #     for proxy in PROXY_SERVERS:
+
+    try:
+        proxy = Proxy("20.210.113.32", 80)
         resp = requests.get(
             url,
             proxies={
-                "http": proxy.get_address(),
-                "https": proxy.get_address(),
+                # "http": proxy.get_address(),
+                # "https": proxy.get_secured_address(),
+                "http": "http://20.210.113.32:80",
+                # "https": "https://20.210.113.32:80",
             },
-            allow_redirects=True,
-            # timeout=3,
+            # allow_redirects=True,
+            timeout=10,
+            verify=False,
             headers={
-                # "X-Requested-With": "XMLHttpRequest",
                 "Accept": "*/*",
                 "Accept-Language": "en-US,en;q=0.9,ko-KR;q=0.8,ko;q=0.7",
                 "Referer": "www.google.com",
@@ -29,3 +35,10 @@ if __name__ == "__main__":
             },
         )
         print(resp.status_code, resp.text)
+
+    except (
+        requests.exceptions.ProxyError,
+        requests.exceptions.SSLError,
+        requests.exceptions.Timeout,
+    ) as e:
+        print(e)
